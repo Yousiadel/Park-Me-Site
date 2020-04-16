6602f30fa16d8512d1e25e04849201f7a0148e27
Paul Louka <38988319+PaulLouka@users.noreply.github.com>
2020-04-16 18:52:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

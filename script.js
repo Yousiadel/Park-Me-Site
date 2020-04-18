0141344aeba559cf8628ac8b6373aae404716052
Paul Louka <38988319+PaulLouka@users.noreply.github.com>
2020-04-17 21:07:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,13 +15,22 @@ $("#register").on("click", function(){
   var email=$("#inputEmail").val()
   var password=$("#inputPassword").val()
   var confirmpassword=$("#inputConfirmPassword").val()
+  var inputPlateNumber=$("#inputPlateNumber").val()
+  var inputMake=$("#inputMake").val()
+  var inputColor=$("#inputColor").val()
   localStorage.setItem("username", username)
   localStorage.setItem("email", email)
   localStorage.setItem("password", password)
   localStorage.setItem("confirmpassword", confirmpassword)
+  localStorage.setItem("inputPlateNumber",inputPlateNumber)
+  localStorage.setItem("inputMake",inputMake)
+  localStorage.setItem("inputColor",inputColor)
   console.log(username);
   console.log(email);
   console.log(password);
   console.log(confirmpassword);
+  console.log(inputPlateNumber);
+  console.log(inputMake);
+  console.log(inputColor);
 })
 
